1f3ed8689886d2ac584bf133a4c64ba09a2564c8
Alberto Talayer <54676308+CarlosTalayer@users.noreply.github.com>
2023-03-01 00:46:36 -0300

    Add files via upload

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Windows.lite\PycharmProjects\django_bem_simples\tutorial-env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
